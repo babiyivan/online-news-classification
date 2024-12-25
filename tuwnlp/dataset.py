d1e4f3459a08a45a9dc3916f87b2b26964e84d71
Cus, Tibor <cus.tibor@outlook.com>
2024-12-25 17:53:18 +0100

    add support for distiled bert tokens

--- tuwnlp/dataset.py
+++ tuwnlp/dataset.py
@@ -38,7 +38,7 @@ class PropagandaDataset(Dataset):
         if tokenizer is not None:
             res = self.tokenizer(self.x.tolist(), return_tensors='pt', padding=True, truncation=True)
             self.attention_mask = res["attention_mask"]
-            self.token_type_ids = res["token_type_ids"]
+            self.token_type_ids = res.get("token_type_ids", [])
             self.input_ids = res["input_ids"]
 
     def __len__(self):
@@ -49,8 +49,9 @@ class PropagandaDataset(Dataset):
             return self.x[index], self.y[index]
         
         return_dict = {
-            "token_type_ids": self.token_type_ids[index,:],
             "attention_mask": self.attention_mask[index,:],
             "input_ids": self.input_ids[index,:],
         }
+        if self.token_type_ids != []:
+            return_dict["token_type_ids"] = self.token_type_ids
         return return_dict, self.y[index]
